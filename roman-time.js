715c1260724c12d28cf54ea09d26152d19cd645c
mironov-alexey <mironov.alexey.fiit.urfu@gmail.com>
2015-10-05 22:04:15 +0500

    Task is completed

--- roman-time.js
+++ roman-time.js
@@ -1,4 +1,12 @@
-var hours = process.argv[2];
-var minutes = process.argv[3];
+if (process.argv.length != 4)
+{
+	console.log("arguments length is incorrect")
+	process.exit(0)
+}
+var hours = parseInt(process.argv[2]);
+var minutes = parseInt(process.argv[3]);
+if (hours > 23 || hours < 0 || minutes > 59 || minutes < 0)
+	console.log("Time is incorrect")
 
-// Немного замечательного кода и магии
+var nums = "- I II III IV V VI VII VIII IX X XI XII XIII XIV XV XVI XVII XVIII XIX XX XXI XXII XXIII XXIV XXV XXVI XXVII XXVIII XXIX XXX XXXI XXXII XXXIII XXXIV XXXV XXXVI XXXVII XXXVIII XXXIX XL XLI XLII XLIII XLIV XLV XLVI XLVII XLVIII XLIX L LI LII LIII LIV LV LVI LVII LVIII LIX".split(' ');
+console.log(nums[hours] + ":" + nums[minutes])
